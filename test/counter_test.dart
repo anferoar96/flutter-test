417eee5e8846f360e936706efade3f59eb5790a5
aromanf96 <aromanf96@gmail.com>
2024-08-20 22:07:52 -0500

    Some unit and integrations tests

---
+++ test/counter_test.dart
@@ -0,0 +1,21 @@
+import 'dart:ffi';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:testing_app/counter.dart';
+
+void main() {
+  group('Counter Tests', () {
+    test('Counter value should be increment', () {
+      final counter = Counter();
+      counter.increment();
+      expect(counter.value, 1);
+    });
+
+    test('Counter value should be decremented', () {
+      final counter = Counter();
+      counter.increment();
+      counter.decrement();
+      expect(counter.value, 0);
+    });
+  });
+}
